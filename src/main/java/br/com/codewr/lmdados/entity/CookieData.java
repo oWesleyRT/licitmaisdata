3d155a06c6e1679e6d74e1d8e64428fea7781286
oWesleyRT <owesleyrt@gmail.com>
2023-09-05 22:39:05 -0300

    hotfix: corrige validação do cookie

--- src/main/java/br/com/codewr/lmdados/entity/CookieData.java
+++ src/main/java/br/com/codewr/lmdados/entity/CookieData.java
@@ -3,27 +3,26 @@ package br.com.codewr.lmdados.entity;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 public class CookieData {
 
     private String cookie;
-    private Integer dayCreate;
-    private Integer monthCreate;
+    private String dateCreate;
+    private String dateExpire;
 
     public CookieData(String cookie){
-        LocalDate localDate = LocalDate.now();
+        LocalDateTime localDateTime = LocalDateTime.now();
         this.cookie = cookie;
-        this.dayCreate = localDate.getDayOfMonth();
-        this.monthCreate = localDate.getMonthValue();
+        this.dateCreate = localDateTime.toString();
+        this.dateExpire = localDateTime.plusHours(12).toString();
     }
 
     public static Boolean cookieIsValid(CookieData cookie){
-        LocalDate localDate = LocalDate.now();
-        Integer dayToday = localDate.getDayOfMonth();
-        Integer monthToday = localDate.getMonthValue();
-        if(cookie.getDayCreate() == dayToday &&
-                cookie.getMonthCreate() == monthToday) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        LocalDateTime dateTimeExpire = LocalDateTime.parse(cookie.getDateExpire());
+        if(localDateTime.isBefore(dateTimeExpire)) {
             return true;
         }
         return false;
